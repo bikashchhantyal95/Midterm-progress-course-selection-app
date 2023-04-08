97e80ff35c6429e9308c57671f446f224caf98f2
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-04-08 19:30:59 -0400

    Initialize project

---
+++ app/src/main/java/com/bikash/cambrain_course_seclection_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bikash.cambrain_course_seclection_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
