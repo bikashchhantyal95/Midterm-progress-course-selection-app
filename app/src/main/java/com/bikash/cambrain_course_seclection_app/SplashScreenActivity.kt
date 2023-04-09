e0b2143a063ef67e74651ef214c23d40f7664cea
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-04-09 17:50:47 -0400

    Created Splash screen and added logo image.

---
+++ app/src/main/java/com/bikash/cambrain_course_seclection_app/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.bikash.cambrain_course_seclection_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
