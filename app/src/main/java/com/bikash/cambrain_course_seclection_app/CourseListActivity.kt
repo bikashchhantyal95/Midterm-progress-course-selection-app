85cdb092b33abfcfe3d730dd089fe56bcd992d56
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-04-19 21:35:24 -0400

    Created CourseListActivity to list courses available.

--- app/src/main/java/com/bikash/cambrain_course_seclection_app/CourseListActivity.kt
+++ app/src/main/java/com/bikash/cambrain_course_seclection_app/CourseListActivity.kt
@@ -3,9 +3,9 @@ package com.bikash.cambrain_course_seclection_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Login : AppCompatActivity() {
+class CourseListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_course_list)
     }
 }
