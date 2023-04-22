3e119d15d47fb4fb622e337a1d2ab0f97b602fb0
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-04-22 00:09:40 -0400

    Created CourseDAO

--- app/src/main/java/com/bikash/cambrain_course_seclection_app/Course.kt
+++ app/src/main/java/com/bikash/cambrain_course_seclection_app/Course.kt
@@ -1,10 +1,17 @@
 package com.bikash.cambrain_course_seclection_app
 
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NonNls
 
+@Entity(tableName = "Course")
+data class Course(
+    @PrimaryKey(autoGenerate = true) val id:Int?,
+    @NonNull @ColumnInfo(name = "code") val code: String,
+    @NonNull @ColumnInfo(name = "name") val name: String,
+    @NonNull @ColumnInfo(name = "description") val description: String,
+    @NonNull @ColumnInfo(name = "professor") val professor: String) {
 
-data class Course(val code: String, val name: String, val description: String, val professor: String) {
-    override fun toString(): String {
-        return "$code : $name"
-    }
 }
--- app/src/main/java/com/bikash/cambrain_course_seclection_app/Database/CourseDAO.kt
+++ app/src/main/java/com/bikash/cambrain_course_seclection_app/Database/CourseDAO.kt
@@ -1,9 +1,23 @@
 package com.bikash.cambrain_course_seclection_app.Database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.bikash.cambrain_course_seclection_app.Course
 
 @Dao
 interface CourseDAO {
+    @Query("SELECT * FROM Course ORDER by name ASC") //select the courses from the database
+    fun getAll(): LiveData<List<Course>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE) // insert the data on the table
+    fun insertCourse(course: Course)
+
+    @Delete
+    fun deleteCourse(course: Course)
+
 
 }
